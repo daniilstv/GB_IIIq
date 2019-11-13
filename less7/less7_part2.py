e6cb8f13868e9b39279bac0794e4eccf9584167a
Daniil Dyachenko <daniilstv@gmail.com>
2019-11-13 22:50:30 +0300

    second part of lesson 7 - final

--- less7/less7_part2.py
+++ less7/less7_part2.py
@@ -1,11 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-import pandas as pd
 from pymongo import MongoClient
 
 '''
@@ -19,19 +14,13 @@ client = MongoClient('mongodb://127.0.0.1:27017')
 db = client['mvideo']
 client.mvideo.docs.drop()
 docs = db.docs
-#
-# def db_mvideo_writer(df):
-#     data = pd.DataFrame()
-#     client.mvideo.docs.drop()
-#     mvideo_db = client['mvideo']
-#     docs = mvideo_db.docs
-#     result = docs.insert_many(df.to_dict(orient='records'))
-#     return result.inserted_ids
+
 
 def get_elems():
-    block = driver.find_element_by_class_name('accessories-product-list')
+    block = driver.find_element_by_css_selector('.gallery-layout.sel-hits-block ')
     elems = block.find_elements_by_class_name('gallery-list-item')
     return elems
+
 
 def collect_data(elems):
     for elem in elems:
@@ -40,18 +29,14 @@ def collect_data(elems):
         result = docs.insert_one({'name': name, 'price': price})
     return print(result)
 
-# chrome_options = Options()
-# chrome_options.add_argument('start-maximized')
-# chrome_options.add_argument('ChromeDriverManager().install()')
-
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.mvideo.ru/')
 
 elems = get_elems()
-
 collect_data(elems)
 
+block = driver.find_element_by_class_name('sel-hits-block')
 buttons = block.find_element_by_class_name('carousel-paging').find_elements_by_tag_name('a')[1:]
 for button in buttons:
     button.click()
@@ -59,3 +44,6 @@ for button in buttons:
     elems = get_elems()
     collect_data(elems)
 
+
+for i in docs.find():
+    print(i)
